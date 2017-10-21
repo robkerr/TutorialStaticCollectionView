42ec64ee01a834d0653643e239aa458c3c853789
Rob Kerr <rob@kerr.cc>
2017-10-21 15:10:43 -0400

    Initial Commit

---
+++ StaticCellCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StaticCellCollectionView
+//
+//  Created by Robert Kerr on 10/21/17.
+//  Copyright © 2017 Robert Kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
