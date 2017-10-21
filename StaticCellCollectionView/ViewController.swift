dcbe7b4a2447bbbf5c2f3bc67c62ea736315cc6d
Rob Kerr <rob@kerr.cc>
2017-10-21 17:23:31 -0400

    Completed project

--- StaticCellCollectionView/ViewController.swift
+++ StaticCellCollectionView/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let cellIds = ["Purple Cell","Green Cell","Blue Cell","Red Cell"]
+    let cellSizes = Array(repeatElement(CGSize(width:170, height:80), count: 4))
+    
+//    let cellSizes = [
+//        CGSize(width:210, height:60),
+//        CGSize(width:180, height:100),
+//        CGSize(width:170, height:80),
+//        CGSize(width:150, height:150)
+//    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("User tapped on \(cellIds[indexPath.row])")
+    }
+}
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellIds.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellIds[indexPath.item], for: indexPath)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSizes[indexPath.item]
+    }
+}
